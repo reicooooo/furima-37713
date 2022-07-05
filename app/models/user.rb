0f967ab9b1642cd6a557006de05197b195061ba1
reicooooo <grqte.1014@gmail.com>
2022-07-06 00:25:09 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many:products 
+         has_many:purchases
+
+         vallidates :nickname, presence: true
+         vallidates :family_name, presence: true
+         vallidates :first_name, presence: true
+         vallidates :family_name_kana, presence: true
+         vallidates :first_name_kana, presence: true
+         vallidates :birthday, presence: true
 end
