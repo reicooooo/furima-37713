8423ae2e4532253211d12dea4a65f49c6a3065e2
reicooooo <grqte.1014@gmail.com>
2022-07-08 13:03:14 +0900

    テストコード修正４

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,7 +104,7 @@ RSpec.describe User, type: :model do
      it "first_nameが空だと登録できない" do 
        @user.first_name = '' 
        @user.valid?
-       expect(@user.errors.full_messages).to include("First name Full=width characters")
+       expect(@user.errors.full_messages).to include("First name can't be blank")
      end
 
      it "family_name_kanaが空だと登録できない" do
