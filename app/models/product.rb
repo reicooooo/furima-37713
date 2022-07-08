ac4fa26ad59f32c6511e75a21ca924b6fd17cb1e
reicooooo <grqte.1014@gmail.com>
2022-07-08 16:37:37 +0900

    バリデーション記述

---
+++ app/models/product.rb
@@ -0,0 +1,15 @@
+class Product < ApplicationRecord
+  belongs_to :user 
+  has_one :purchase  
+
+  validates :name, presence: true
+  validates :price,presence: true
+  validates :description ,presence: true
+  validates :condition_id,presence: true
+  validates :sipping_postage_id,presence: true
+  validates :sipping_day_id,presence: true
+  validates :category_id,presence: true
+  validates :sipping_id ,presence: true
+  validates :user,presence: true
+end
+
