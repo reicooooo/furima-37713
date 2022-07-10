66275dfe034440e320883316c6d43899f8114071
reicooooo <grqte.1014@gmail.com>
2022-07-10 16:57:18 +0900

    jsの実装

--- app/javascript/packs/product_price.js
+++ app/javascript/packs/product_price.js
@@ -1,15 +1,11 @@
 window.addEventListener('load', () => {
-  console.log("OK");
-});
-
-window.addEventListener('load', () => {
 const priceInput = document.getElementById("item-price");
-console.log(priceInput);
-});
-
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-   console.log("イベント発火");
- })
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   console.log(inputValue);
+   const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1);
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1))
+})
 });
